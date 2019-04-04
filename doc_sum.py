6947f2a5fbbc012c031c2e566d489e99b846b985
Dima T <mrlocky98@gmail.com>
2019-04-04 15:44:36 +0300

    skeleton added

---
+++ doc_sum.py
@@ -0,0 +1,6 @@
+def launch():
+    print("Doc sum launcher")
+
+
+if __name__ == '__main__':
+    launch()
---
+++ query_exp.py
@@ -0,0 +1,6 @@
+def launch():
+    print("Query expansion launcher")
+
+
+if __name__ == '__main__':
+    launch()
